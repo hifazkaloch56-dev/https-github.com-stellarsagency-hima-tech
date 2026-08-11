24485ff98a6dd9925f122d22cac90347e6e41cb6
hifazkaloch56-dev <hifazkaloch56@gmail.com>
2026-08-11 07:07:50 -0700

    Add XML sitemap route

--- server.js
+++ server.js
@@ -182,6 +182,15 @@ app.get('/robots.txt', (req, res) => {
     'Sitemap: https://himatechrcm.com/sitemap.xml\n'
   );
 });
+app.get('/sitemap.xml', (req, res) => {
+  res.type('application/xml');
+  res.send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://www.himatechrcm.com/</loc>
+  </url>
+</urlset>`);
+});
 
 /* ------------------------------------------------------------
    ERROR HANDLING
